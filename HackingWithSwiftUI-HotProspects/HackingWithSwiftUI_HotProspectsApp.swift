535b726700ed911874ea3f61fe78d2da8626383c
Michael Jones <michael.jones90@me.com>
2026-07-20 13:37:20 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-HotProspects/HackingWithSwiftUI_HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_HotProspectsApp.swift
+//  HackingWithSwiftUI-HotProspects
+//
+//  Created by Michael Jones on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
