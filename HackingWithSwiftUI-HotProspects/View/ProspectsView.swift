011c6a260e7534ae5501fd513e166ac9286094bf
Michael Jones <michael.jones90@me.com>
2026-07-26 09:11:53 +0100

    extra comments.

--- HackingWithSwiftUI-HotProspects/View/ProspectsView.swift
+++ HackingWithSwiftUI-HotProspects/View/ProspectsView.swift
@@ -126,36 +126,43 @@ struct ProspectsView: View {
         }
     }
     
+    /// Deletes all the currently selected prospects from the data model.
     func delete() {
         for prospect in selectedProspects {
             modelContext.delete(prospect)
         }
     }
     
+    /// Schedules a local notification to remind the user to contact a specific Prospect.
     func addNotification(for prospect: Prospect) {
+        /// Fetches the shared notification center for scheduling and managing notifications.
         let centre = UNUserNotificationCenter.current()
         
+        /// Prepares and schedules the notification.
         let addRequest = {
+            /// Sets the notification content (title, subtitle and sound).
             let content = UNMutableNotificationContent()
             content.title = "Contact \(prospect.name)"
             content.subtitle = prospect.email
             content.sound = UNNotificationSound.default
             
+            /// Creates a trigger that is scheduled for 9am.
             var dateComponents = DateComponents()
             dateComponents.hour = 9
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
             
-//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            
+            /// Creates a new request and adds it to the notification center.
             let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-            
             centre.add(request)
         }
         
+        /// Checks if the app is already authorised to show notifications.
         centre.getNotificationSettings { settings in
+            /// Schedules the notification.
             if settings.authorizationStatus == .authorized {
                 addRequest()
             } else {
+                /// Requests permission from the user. If granted, schedules the notification, if not, prints an error.
                 centre.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
                     if success {
                         addRequest()
@@ -165,7 +172,6 @@ struct ProspectsView: View {
                 }
             }
         }
-        
     }
 }
 
